0500f35ebfbc7e111c92c777175d92903589eccc
elianeesvida <a20213997@pucp.edu.pe>
2026-06-21 02:27:06 -0500

    Importamos modulos de las caracteriasticas de la vivienda y del hogar y crianza de mascotas en el hogar de la ENAHO 2025

--- scripts/01_Carga_union_modulos.R
+++ scripts/01_Carga_union_modulos.R
@@ -10,4 +10,8 @@ library(rio)
 library(tidyverse)
 library(janitor)
 library(readr)
+renv::snapshot()
 
+#2. Importar datos--------------------
+mod100 <- import("datos/crudos/Enaho01-2025-100.csv", encoding = "Latin-1")
+mod118 <- import("datos/crudos/Enaho01-2025-118.csv", encoding = "Latin-1")

fb5a8a0a46a22a36f084a96e3525d0a137942133
elianeesvida <a20213997@pucp.edu.pe>
2026-06-21 02:01:30 -0500

    Creamos script para carga y unión de módulos (1 y 2081) de ENAHO 2025

---
+++ scripts/01_Carga_union_modulos.R
@@ -0,0 +1,14 @@
+#================================================================================
+#Proyecto: Mascotas ENAHO 2025
+#Script: Cargar los módulos y hacer los joins
+#Autor: Eliane Caceres
+#Fecha: 21-06-2026
+#===============================================================================
+
+#1.Carga de librerías---------------------------
+library(rio)
+library(tidyverse)
+library(janitor)
+library(readr)
+renv::snapshot()
+

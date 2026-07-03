f21671d0b724bfa0d4cf49de3ad079c3995354e7
elianeesvida <a20213997@pucp.edu.pe>
2026-07-03 01:31:15 -0500

    Carga de la libreria que usaré

---
+++ scripts/02_Acondicionamiento.R
@@ -0,0 +1,27 @@
+# ==============================================================================
+# Proyecto:  Análisis de la tenencia de mascotas y condiciones de vivienda en el Perú
+# Script: Acondicionamiento
+# Autor: Eliane Caceres
+# Fecha: 03-07-2026
+# Objetivo: Acondicionar la base de datos de mascotas (Carga, Selección,
+#           Renombrado, Diagnóstico y Tratamiento de NAs).
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# 0. CONFIGURACIÓN DEL ENTORNO--------------------------------------------------
+# ------------------------------------------------------------------------------
+library(tidyverse)
+library(arrow)
+library(janitor)
+library(naniar)
+renv::snapshot()
+
+
+
+
+
+
+
+
+
+
